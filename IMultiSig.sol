a186fdc05ae43585c379b66aab1f7fe02660f1b8
Sagor <161376518+0xsagor@users.noreply.github.com>
2026-02-06 11:31:19 +0600

    Create IMultiSig.sol

---
+++ IMultiSig.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IMultiSig {
+    event Deposit(address indexed sender, uint256 amount, uint256 balance);
+    event SubmitTransaction(address indexed owner, uint256 indexed txIndex, address indexed to, uint256 value, bytes data);
+    event ConfirmTransaction(address indexed owner, uint256 indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint256 indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint256 indexed txIndex);
+}
